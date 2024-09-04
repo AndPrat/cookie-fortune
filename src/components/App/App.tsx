c4dbff33ff6a8c28df62a3ee3af2e13e6964186e
Andrea Prat <andr8ide@gmail.com>
2024-09-04 09:43:48 +0200

    Refactor Homepage path with LazyHomepage in App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,22 @@
-import HomePage from "../../pages/Homepage/Homepage";
+import { Suspense } from "react";
 import paths from "../../routers/paths/paths";
 import "./App.css";
 import { Navigate, Route, Routes } from "react-router-dom";
+import { LazyHomepage } from "../../pages/LazyPages/LazyPages";
 
 const App = (): React.ReactElement => {
   return (
     <div>
       <main>
         <Routes>
-          <Route path={paths.homePage} element={<HomePage />} />
+          <Route
+            path={paths.homePage}
+            element={
+              <Suspense>
+                <LazyHomepage />
+              </Suspense>
+            }
+          />
           <Route path="/" element={<Navigate to={paths.homePage} />} />
         </Routes>
       </main>
